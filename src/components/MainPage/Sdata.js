38723ae8e99c1f6475ad9c5aae64fcf72e4904e7
Abbinavu <103304516+Abbinavu@users.noreply.github.com>
2024-10-07 11:19:34 +0530

    Add files via upload

---
+++ src/components/MainPage/Sdata.js
@@ -0,0 +1,27 @@
+const Sdata = [
+  {
+    id: 1,
+    title: "50% Off For Your First Shopping",
+    desc: "Timeless elegance meets modern innovation. Our watch collection is more than just a way to keep track of time—it’s a statement of style, precision, and sophistication. Whether you’re dressing up for a special occasion or adding a touch of class to your everyday look, our watches are crafted to complement your unique style and keep you on time, every time.",
+    cover: "./images/SlideCard/slide-1.png",
+  },
+  {
+    id: 2,
+    title: "50% Off For Your First Shopping",
+    desc: "Step into style and comfort with every stride! Our shoe collection combines cutting-edge design with premium materials to keep you looking sharp and feeling great all day long. From sleek sneakers to elegant formal wear, find the perfect pair to elevate your look and take on any occasion with confidence. Walk your way, in shoes that make a statement!",
+    cover: "./images/SlideCard/slide-2.png",
+  },
+  {
+    id: 3,
+    title: "50% Off For Your First Shopping",
+    desc: "Redefine your wardrobe with shirts that speak style and sophistication. From crisp classics to bold prints, our collection is designed to make you stand out effortlessly. Whether it’s a power meeting or a casual day out, find the perfect fit to showcase your unique personality. Dress to impress, every time!",
+    cover: "./images/SlideCard/slide-3.png",
+  },
+  {
+    id: 4,
+    title: "50% Off For Your First Shopping",
+    desc: "Unlock elegance with our enchanting Pandora collection—each piece tells a story, captures a memory, and celebrates your unique style. Whether it's a stunning charm or a dazzling bracelet, express yourself with jewelry that’s as unforgettable as you are. Discover the magic of Pandora and let every moment shine!",
+    cover: "./images/SlideCard/slide-4.png",
+  },
+]
+export default Sdata
---
+++ src/components/shops/Sdata.js
@@ -0,0 +1,69 @@
+const Sdata = {
+  shopItems: [
+    {
+      id: 7,
+      cover: "./images/shops/shops-1.png",
+      name: "Mapple Earphones",
+      price: "180",
+      desc: "Experience sound like never before with Mapple Earphones—crafted for the ultimate audio experience. With crystal-clear sound quality, deep bass, and a sleek ergonomic design, these earphones are built to deliver comfort and precision. Whether it’s your favorite playlist or an important call, immerse yourself in a world of perfect sound, anytime, anywhere.",
+      discount: "25",
+    },
+    {
+      id: 8,
+      cover: "./images/shops/shops-2.png",
+      name: "Vivo android one",
+      price: "120",
+      discount: "10",
+    },
+    {
+      id: 9,
+      cover: "./images/shops/shops-3.png",
+      name: "Sony Light",
+      price: "20",
+      discount: "50 ",
+    },
+    {
+      id: 10,
+      cover: "./images/shops/shops-4.png",
+      name: "Iphone",
+      price: "999",
+      discount: "10 ",
+    },
+    {
+      id: 11,
+      cover: "./images/shops/shops-5.png",
+      name: "Ceats wireless earphone",
+      price: "80",
+      discount: "20 ",
+    },
+    {
+      id: 12,
+      cover: "./images/shops/shops-7.png",
+      name: "Redimi Phone",
+      price: "400",
+      discount: "20 ",
+    },
+    {
+      id: 13,
+      cover: "./images/shops/shops-8.png",
+      name: "Xeats Bluetooth earphones",
+      price: "60",
+      discount: "5 ",
+    },
+    {
+      id: 14,
+      cover: "./images/shops/shops-9.png",
+      name: "Airpod",
+      price: "120",
+      discount: "10",
+    },
+    {
+      id: 15,
+      cover: "./images/shops/shops-10.png",
+      name: "Silver Cap",
+      price: "5",
+      discount: "2",
+    },
+  ],
+}
+export default Sdata
